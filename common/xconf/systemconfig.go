052c4cc5682d5c98c8e700d881678bc4c2b58301
马守越 <showurl@163.com>
2022-11-09 19:16:35 +0800

    add systemconfig, used when the program is running

--- common/xconf/systemconfig.go
+++ common/xconf/systemconfig.go
@@ -46,7 +46,9 @@ type SystemConfigMgr struct {
 }
 
 func NewSystemConfigMgr(namespace string, serviceName string, c *qmgo.Collection) *SystemConfigMgr {
-	return &SystemConfigMgr{Namespace: namespace, ServiceName: serviceName, c: c}
+	s := &SystemConfigMgr{Namespace: namespace, ServiceName: serviceName, c: c}
+	s.initData()
+	return s
 }
 
 func (s *SystemConfigMgr) Get(key string) (value string) {
@@ -143,4 +145,36 @@ func (s *SystemConfigMgr) GetSliceCtx(ctx context.Context, key string) []string 
 	res := make([]string, 0)
 	_ = json.Unmarshal([]byte(config.Value), &res)
 	return res
+}
+
+func (s *SystemConfigMgr) initData() {
+	ctx := context.Background()
+	s.c.InsertOne(ctx, &SystemConfig{
+		Namespace:    "system",
+		ServiceName:  "user",
+		Key:          "signature.if_not_set",
+		Value:        "这个人很懒，还没有设置签名哦～",
+		InputOptions: nil,
+	})
+	s.c.InsertOne(ctx, &SystemConfig{
+		Namespace:    "system",
+		ServiceName:  "user",
+		Key:          "nickname.default",
+		Value:        "XX用户",
+		InputOptions: nil,
+	})
+	s.c.InsertOne(ctx, &SystemConfig{
+		Namespace:    "system",
+		ServiceName:  "user",
+		Key:          "avatars.default",
+		Value:        `["https://go-zero.dev/img/footer/go-zero.svg"]`,
+		InputOptions: nil,
+	})
+	s.c.InsertOne(ctx, &SystemConfig{
+		Namespace:    "system",
+		ServiceName:  "relation",
+		Key:          "friend_max_count",
+		Value:        `20000`,
+		InputOptions: nil,
+	})
 }
