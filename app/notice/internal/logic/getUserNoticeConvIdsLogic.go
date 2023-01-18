5ec66be69368352dcc497f7f4a079a6565c45175
马守越 <showurl@163.com>
2023-01-18 18:59:22 +0800

    Update getUserNoticeConvIdsLogic.go

--- app/notice/internal/logic/getUserNoticeConvIdsLogic.go
+++ app/notice/internal/logic/getUserNoticeConvIdsLogic.go
@@ -28,23 +28,46 @@ func NewGetUserNoticeConvIdsLogic(ctx context.Context, svcCtx *svc.ServiceContex
 func (l *GetUserNoticeConvIdsLogic) GetUserNoticeConvIds(in *pb.GetUserNoticeConvIdsReq) (*pb.GetUserNoticeConvIdsResp, error) {
 	var convIds = noticemodel.DefaultConvIds
 	// 获取用户的所有好友id
-	getFriendList, err := l.svcCtx.RelationService().GetFriendList(l.ctx, &pb.GetFriendListReq{
-		CommonReq: &pb.CommonReq{
-			UserId: in.UserId,
-		},
-		Page: &pb.Page{
-			Page: 1,
-			Size: 999999,
-		},
-		Opt: pb.GetFriendListReq_OnlyId,
-	})
-	if err != nil {
-		l.Errorf("get friend list failed, err: %v", err)
-		return &pb.GetUserNoticeConvIdsResp{CommonResp: pb.NewRetryErrorResp()}, err
+	{
+		getFriendList, err := l.svcCtx.RelationService().GetFriendList(l.ctx, &pb.GetFriendListReq{
+			CommonReq: &pb.CommonReq{
+				UserId: in.UserId,
+			},
+			Page: &pb.Page{
+				Page: 1,
+				Size: 999999,
+			},
+			Opt: pb.GetFriendListReq_OnlyId,
+		})
+		if err != nil {
+			l.Errorf("get friend list failed, err: %v", err)
+			return &pb.GetUserNoticeConvIdsResp{CommonResp: pb.NewRetryErrorResp()}, err
+		}
+		friends := getFriendList.Ids
+		for _, friend := range friends {
+			convIds = append(convIds, noticemodel.ConvIdUser(friend))
+		}
 	}
-	friends := getFriendList.Ids
-	for _, friend := range friends {
-		convIds = append(convIds, noticemodel.ConvIdUser(friend))
+	// 获取用户的所有群组id
+	{
+		getGroupList, err := l.svcCtx.GroupService().GetMyGroupList(l.ctx, &pb.GetMyGroupListReq{
+			CommonReq: &pb.CommonReq{
+				UserId: in.UserId,
+			},
+			Page: &pb.Page{
+				Page: 1,
+				Size: 999999,
+			},
+			Opt: pb.GetMyGroupListReq_ONLY_ID,
+		})
+		if err != nil {
+			l.Errorf("get group list failed, err: %v", err)
+			return &pb.GetUserNoticeConvIdsResp{CommonResp: pb.NewRetryErrorResp()}, err
+		}
+		groups := getGroupList.Ids
+		for _, group := range groups {
+			convIds = append(convIds, noticemodel.ConvIdGroup(group))
+		}
 	}
 	return &pb.GetUserNoticeConvIdsResp{
 		ConvIds: utils.Set(convIds),
