dba8893f3af4c72a8288899fca29e6aedf5c7135
马守越 <showurl@163.com>
2023-02-13 23:27:36 +0800

    update redis key

--- common/xredis/rediskey/user.go
+++ common/xredis/rediskey/user.go
@@ -19,3 +19,17 @@ func DeviceUserMapping(deviceId string) string {
 func UserDeviceMappingExpire() int64 {
 	return 60 * 60 * 24 * 30
 }
+
+func RegisterIpLimitKey(ip string) string {
+	return "register_ip_limit:" + ip
+}
+
+// UserPasswordErrorCountKey 用户密码输入错误次数
+func UserPasswordErrorCountKey(uid string) string {
+	return "user_password_error_count:" + uid
+}
+
+// LatestTurnConvIdKey 上次轮到的会话id
+func LatestTurnConvIdKey(invitationCode string) string {
+	return "latest_turn_conv_id:" + invitationCode
+}
